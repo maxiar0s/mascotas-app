9c60dcc1d2adf4dfc812ab16852826f9c1b6f64d
Maximiliano Aros <maxi.aros.osorio@gmail.com>
2025-02-21 12:22:34 -0300

    primeros componentes y grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { MascotaComponent } from './components/mascota/mascota.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, MascotaComponent, FooterComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <div class="" id="container">
+      <app-nav id="nav" class="bg-blue-900"></app-nav>
+
+      <app-mascota id="mascota-content" class="bg-red-900"></app-mascota>
+
+      <app-footer id="footer" class="bg-green-900"></app-footer>
+    </div>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+      #container {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: 1fr 10fr 1fr;
+        min-height: 100vh;
+        grid-template-areas:
+          'nav nav nav'
+          'mascota mascota mascota'
+          'footer footer footer';
+      }
+      #nav {
+        grid-area: nav;
+      }
+      #mascota-content {
+        grid-area: mascota;
+      }
+      #footer {
+        grid-area: footer;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'mascotas-app';
---
+++ src/app/components/mascota/mascota.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascota',
+  imports: [],
+  template: `
+    <p>
+      mascota works!
+    </p>
+  `,
+  styles: ``
+})
+export class MascotaComponent {
+
+}
